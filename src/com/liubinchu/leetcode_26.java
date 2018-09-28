e2786ca1ebf500c8aed5299b3b3d31370b141dd9
liubinchu <32641420+liubinchu@users.noreply.github.com>
2018-09-28 15:06:01 +0800

    leetcode26

---
+++ src/com/liubinchu/leetcode_26.java
@@ -0,0 +1,15 @@
+package com.liubinchu;
+
+public class leetcode_26 {
+//从排序数组中删除重复项
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i=1 ; i<nums.length; i++){
+            if( nums[i] != nums[index]){
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+        return index+1;
+    }
+}
