5d85a61168286a3890e6bf5c01f95c7171e9492c
liubinchu <32641420+liubinchu@users.noreply.github.com>
2018-11-12 21:02:20 +0800

    10.28/2018

---
+++ src/com/leetcode/OfferSword01.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import java.math.BigDecimal;
+
+/**
+ * @author liubi
+ */
+public class OfferSword01 {
+    public static void main(String[] args) {
+        OfferSword01 solution = new OfferSword01();
+        for(String key :System.getenv().keySet()){
+            System.out.println(System.getenv(key));
+        }
+        System.getProperties().list(System.out);
+
+    }
+}
