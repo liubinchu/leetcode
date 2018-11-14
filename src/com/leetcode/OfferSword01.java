1b46359513d0226174a214d27d447aee7dcfea49
liubinchu <32641420+liubinchu@users.noreply.github.com>
2018-11-14 13:02:37 +0800

    11.14/2018

--- src/com/leetcode/OfferSword01.java
+++ src/com/leetcode/OfferSword01.java
@@ -1,16 +1,22 @@
 package com.leetcode;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * @author liubi
  */
 public class OfferSword01 {
     public static void main(String[] args) {
-        OfferSword01 solution = new OfferSword01();
-        for(String key :System.getenv().keySet()){
-            System.out.println(System.getenv(key));
+        label:
+        for (int i =0; i<100;i++){
+            for (int j=0;j<10;j++){
+                if(j==5) continue label;
+                if(j==9) break label;
+                System.out.println(i*j);
+            }
         }
-        System.getProperties().list(System.out);
+        TreeNode treeNode = new TreeNode(1);
 
     }
 }
--- src/com/leetcode/TreeNode.java
+++ src/com/leetcode/TreeNode.java
@@ -1,8 +1,21 @@
 package com.leetcode;
 
-public class TreeNode {
+/**
+ * @author liubi
+ */
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this(val);
+        this.left = left;
+        this.right = right;
+    }
+
 }
