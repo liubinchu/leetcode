655100bb360cd6cff6a22f3fde14bd8d3afdfad3
liubinchu <32641420+liubinchu@users.noreply.github.com>
2018-12-17 09:45:31 +0800

    12.17/2018

--- src/com/leetcode/OfferSord45_35.java
+++ src/com/leetcode/OfferSord45_35.java
@@ -25,6 +25,12 @@ class IntegerStringOrderCMP implements Comparator<Integer>{
         return 0; // 完全相等
     }
 }
+
+/**
+ * 题解: 1. 对于两个数字，其两种排列情况 长度都相等， 对于多个数字， 其所有排列的总长度也相等，
+ * 因此对于多个数字进行排列 求的最小值的过程 可以 退化成 将数字两两按照组合成的最小值进行排序。（两两排序的结果 能够得到多个数字排序最小值 需要证明 自反性，对称性，以及传递性）
+ * 2. 如果两个数字的拼接 结果 超过 int/long 范围， 如果使用 double 会损失精度，一个常用的做法 是比较其字符串中的字符顺序（！！实用！！）
+ */
 public class OfferSord45_35 {
     public String PrintMinNumber(int [] numbers) {
         StringBuilder stringBuilder = new StringBuilder();
