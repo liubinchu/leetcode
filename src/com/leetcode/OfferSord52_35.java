265dc0a4f8f8bb57ae6e4ab4d46e3d7c5ffa1fa7
liubinchu <32641420+liubinchu@users.noreply.github.com>
2018-12-27 16:25:32 +0800

    12.27/2018

---
+++ src/com/leetcode/OfferSord52_35.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * @author liubi
+ * @date 2018-12-27 10:53
+ **/
+public class OfferSord52_35 {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        return pHead1;
+    }
+    public static void main(String[] args) {
+        OfferSord52_35 solution = new OfferSord52_35();
+    }
+}
