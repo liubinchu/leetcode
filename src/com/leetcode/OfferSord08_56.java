62d021c579024c89f74f4564c28a5304a654c373
liubinchu <32641420+liubinchu@users.noreply.github.com>
2019-01-08 20:45:37 +0800

    Jan/8/2019

--- src/com/leetcode/OfferSord08_56.java
+++ src/com/leetcode/OfferSord08_56.java
@@ -4,8 +4,8 @@ package com.leetcode;
  * @author liubi
  * @date 2019-01-08 12:39
  **/
-public class OfferSord8_56 {
+public class OfferSord08_56 {
     public static void main(String[] args) {
-        OfferSord8_56 solution = new OfferSord8_56();
+        OfferSord08_56 solution = new OfferSord08_56();
     }
 }
--- src/com/leetcode/OfferSord19_51.java
+++ src/com/leetcode/OfferSord19_51.java
@@ -5,11 +5,67 @@ package com.leetcode;
  * @date 2019-01-08 12:35
  **/
 public class OfferSord19_51 {
+    /**
+     * 算法思想：见剑指offer 实际上是 NFA 非确定有限状态自动机
+     */
+    private char[] str ;
+    private char[] pattern ;
     public boolean match(char[] str, char[] pattern)
     {
-        return false;
+        if(str==null||pattern==null){
+            return false;
+        }
+        else{
+            this.pattern = pattern;
+            this.str = str;
+            return NFA(0,0);
+        }
+    }
+    private boolean NFA(int strIndex, int patternIndex){
+        if(strIndex==str.length&&patternIndex==pattern.length){
+            return true;
+        }
+        else if(strIndex==str.length&&patternIndex<pattern.length){
+            if(patternIndex+2==pattern.length && pattern[patternIndex+1] =='*'){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if(strIndex<str.length&&patternIndex==pattern.length){
+            return false;
+        }
+        char ch = str[strIndex];
+        char pa = pattern[patternIndex];
+        boolean anyAmount = false;
+        if(patternIndex+1<pattern.length && pattern[patternIndex+1] =='*'){
+            anyAmount = true;
+        }
+        if(!anyAmount){
+            if(ch==pa||pa == '.'){
+                return NFA(strIndex+1,patternIndex+1);
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            // anyAmount == true
+            if(ch==pa||pa == '.'){
+                return NFA(strIndex,patternIndex+2)||
+                        NFA(strIndex+1,patternIndex+2)||
+                        NFA(strIndex+1,patternIndex);
+            }
+            else {
+                return NFA(strIndex,patternIndex+2);
+            }
+        }
     }
     public static void main(String[] args) {
         OfferSord19_51 solution = new OfferSord19_51();
+        char[] str = {};
+        char[] pattern = {'.','*'};
+        System.out.println(solution.match(str,pattern));
     }
 }
