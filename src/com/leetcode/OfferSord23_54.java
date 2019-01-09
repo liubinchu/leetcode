d9f975225db860619daa603571f571786dc321c2
liubinchu <32641420+liubinchu@users.noreply.github.com>
2019-01-09 22:30:21 +0800

    Jan/9/2019

--- src/com/leetcode/OfferSord23_54.java
+++ src/com/leetcode/OfferSord23_54.java
@@ -8,7 +8,12 @@ import java.util.Set;
  * @date 2019-01-08 12:37
  **/
 public class OfferSord23_54 {
-    public ListNode EntryNodeOfLoop(ListNode pHead)
+    /**
+     * 算法分析: 时间复杂度 O（n） 空间复杂度 O（n）
+     * @param pHead
+     * @return
+     */
+    public ListNode EntryNodeOfLoop1(ListNode pHead)
     {
         Set<ListNode> appearedNode = new HashSet<>();
         ListNode pointer = pHead;
@@ -22,6 +27,52 @@ public class OfferSord23_54 {
             pointer = pointer.next;
         }
         return pointer; // 当不存在环时，能够正常跳出循环，pointer == null
+    }
+
+    public ListNode EntryNodeOfLoop(ListNode pHead){
+        if(pHead == null){
+            // null element
+            return null;
+        }
+        else if(pHead.next == null){
+            // only one element
+            return null;
+        }
+
+        // define whether exists loop
+        ListNode p1 = pHead;
+        ListNode p2 = pHead.next.next;
+        boolean hasLoop = false;
+        while (p1!=null &&p2!=null&&p2.next !=null){
+            if(p2 ==p1){
+                hasLoop = true;
+                break;
+            }
+            else if (p2.next == p1){
+                hasLoop = true;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        if(!hasLoop){
+            return null;
+        }
+        else {
+            // define the length of loop/ number of element in loop
+            p2 = p1.next;
+            int length = 1; // init with 1;
+            while (p2!=p1){
+                p2 = p2.next;
+                length++;
+            }
+            // define the start element of the loop
+            // loop 长度 为 length , 起始部分/非loop部分 长度 为 X
+            // p1 p2 init with pHead
+            // p1 先走 length 步， 此时 p1，p2 同时向前走  设 走N步
+            // 因为 存在loop p2 定能 追上p1
+            // 当第一次相遇（此后进入循环 p1===p2），
+        }
     }
     public static void main(String[] args) {
         ListNode p1 = new ListNode(1);
