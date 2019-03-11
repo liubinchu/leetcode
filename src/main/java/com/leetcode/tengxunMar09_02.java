836b2f0b75f0868b28523879e77ff42782944f61
liubinchu <liubinchu@outlook.com>
2019-03-11 16:25:10 +0800

    Mar/11/2019

--- src/main/java/com/leetcode/tengxunMar09_02.java
+++ src/main/java/com/leetcode/tengxunMar09_02.java
@@ -5,21 +5,22 @@ package com.leetcode;
  * @date 2019-03-09 20:03
  **/
 public class tengxunMar09_02 {
-    private void fun(int start, int end) {
+    private int  fun(int start, int end) {
         int sum = 0;
         sum = (end - start+1) / 2;
+        // 两两组合
         if(start%2==0){
             sum = -sum;
         }
+        // 每个组合的结果
         int residue = (end - start+1) % 2;
-        if (residue == 0) {
-            System.out.println(sum);
-        } else {
-            int num1 = end;
-            int num2 = end % 2 == 0 ? 1 : -1;
-            sum = sum + num1 * num2;
+        // 是否存在数子 不在组合中
+        if (residue != 0) {
+            int symbol = end % 2 == 0 ? 1 : -1;
+            sum = sum + end * symbol;
             System.out.println(sum);
         }
+        return sum;
     }
     public static void main(String[] args) {
         tengxunMar09_02 solusion = new tengxunMar09_02();
