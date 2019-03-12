d86609cac3c68ae113d9aa18867714b59b63bcbf
liubinchu <liubinchu@outlook.com>
2019-03-12 21:46:31 +0800

    Mar/12/2019

---
+++ src/main/java/com/leetcode/YuanJing02.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import java.util.Scanner;
+
+/**
+ * @author liubi
+ * @date 2019-03-12 21:35
+ **/
+public class YuanJing02 {
+    // https://www.nowcoder.com/questionTerminal/06a571f131424c2dbfb2195030c630da
+    // 背包问题
+    public static void homework(){
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        while (in.hasNext()){
+            int t = in.nextInt();
+            int v = in.nextInt();
+
+            if(t==0 && v==0){
+                break;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        YuanJing02 solution = new YuanJing02();
+    }
+}
