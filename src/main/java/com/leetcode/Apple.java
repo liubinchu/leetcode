43093e58f9132f25e36759881cf3c17754b8b0b7
liubinchu <liubinchu@outlook.com>
2019-03-24 20:59:02 +0800

    Mar/24/2019

--- src/main/java/com/leetcode/Apple.java
+++ src/main/java/com/leetcode/Apple.java
@@ -1,8 +1,14 @@
 package com.leetcode;
 
+import org.apache.logging.log4j.core.config.AppendersPlugin;
+
+import java.math.BigDecimal;
+import java.security.AllPermission;
+
 /**
  * @author liubi
  * @date 2019-03-17 22:43
  **/
-public class Apple extends Fruit{
+public class Apple extends Exception{
 }
+
