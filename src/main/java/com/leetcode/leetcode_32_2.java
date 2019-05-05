c00a8e03b9e11b750b5e81646f18abe9dae0640e
liubinchu <liubinchu@outlook.com>
2019-05-05 22:26:12 +0800

    May/5/2019

--- src/main/java/com/leetcode/leetcode_32_2.java
+++ src/main/java/com/leetcode/leetcode_32_2.java
@@ -6,27 +6,29 @@ package com.leetcode;
  **/
 public class leetcode_32_2 {
     /**
-     * 没弄懂 时间复杂度 和 空间复杂度 同 使用栈相同
+     * 时间复杂度 和 空间复杂度 同 使用栈相同
+     * leetcode 官网中题解
      * @param s
      * @return
      */
+    private int[] dp ;
+    // dp[i] 表示当 i==')'时，当前最长括号串的长度， i=='('时，永远为0
     public int longestValidParentheses(String s) {
-        int sz = s.length();
-        if (sz < 2) {
-            return 0;
-        }
-        int maxLen = 0;
-        int[] dp = new int[sz + 1];
-        for (int i = 1; i < sz + 1; i++) {
-            int j = i - 2 - dp[i - 1];
-            if (s.charAt(i - 1) == '(' || j < 0 || s.charAt(j) == ')') {
-                dp[i] = 0;
-            } else {
-                dp[i] = dp[i - 1] + 2 + dp[j];
-                maxLen = Math.max(maxLen, dp[i]);
-            }
-        }
-        return maxLen;
+       this.dp = new int[s.length()];
+       int maxLen = Integer.MIN_VALUE;
+       for(int i=1;i<s.length();i++){ // 第一个一定不配对
+           if (s.charAt(i)==')'){
+               if (s.charAt(i-1)=='('){ // 相邻的两个能够配对
+                   this.dp[i] = (i>=2?this.dp[i-2]:0) + 2; // 之前的串长度 加上 2
+                   if(this.dp[i]>maxLen){maxLen = this.dp[i];}
+               }
+               else if(i-this.dp[i-1]-1>=0 && s.charAt(i-1)==')' && s.charAt(i-this.dp[i-1]-1)=='('){ //嵌套的括号串
+                   this.dp[i] = dp[i-1] + 2 + (i-2-this.dp[i-1]>=0?this.dp[i-2-this.dp[i-1]]:0); // 前一个串的长度 + 2 + 之前串的长度
+                   if(this.dp[i]>maxLen){maxLen = this.dp[i];}
+               }
+           }
+       }
+       return maxLen;
     }
 
     public static void main(String[] args) {
